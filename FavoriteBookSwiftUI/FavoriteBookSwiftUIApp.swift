30f170b0f721a816f0eab7971f158db6af0ac65e
Zeynep Bayrak Demirel <zeynepbayrak863@gmail.com>
2023-07-13 15:03:09 +0300

    Initial Commit

---
+++ FavoriteBookSwiftUI/FavoriteBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookSwiftUIApp.swift
+//  FavoriteBookSwiftUI
+//
+//  Created by Zeynep Bayrak Demirel on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
